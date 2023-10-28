a87cd7f96e41ec377201db6e5e36101e50cbe617
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-10-28 03:31:45 -0700

    [add]: loging errors

--- src/pages/api/pdf2text.ts
+++ src/pages/api/pdf2text.ts
@@ -3,7 +3,9 @@ import { createWorker } from 'tesseract.js';
 import * as pdfjs from 'pdfjs-dist';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
+function isError(error: unknown): error is Error {
+    return error instanceof Error;
+  }
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).end(); // Method Not Allowed
@@ -19,37 +21,54 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const text = await extractTextFromPDF(pdfFileBuffer);
     res.status(200).json({ text });
   } catch (err) {
-    res.status(500).json({ error: 'Failed to process PDF.' });
+    console.error(err); // Log the error for debugging
+    
+    const errorMessage = isError(err) ? err.message : 'An unknown error occurred';
+    res.status(500).json({ error: `Failed to process PDF: ${errorMessage}` });
   }
 }
 
 async function extractTextFromPDF(pdfBuffer: Buffer): Promise<string> {
-  const loadingTask = pdfjs.getDocument(new Uint8Array(pdfBuffer));
-  const pdf = await loadingTask.promise;
-  const numPages = pdf.numPages;
-  let extractedText = '';
+  try {
+    const loadingTask = pdfjs.getDocument(new Uint8Array(pdfBuffer));
+    const pdf = await loadingTask.promise;
+    const numPages = pdf.numPages;
+    let extractedText = '';
 
-  for (let pageNumber = 1; pageNumber <= numPages; pageNumber++) {
-    const page = await pdf.getPage(pageNumber);
-    const viewport = page.getViewport({ scale: 1.0 });
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+    for (let pageNumber = 1; pageNumber <= numPages; pageNumber++) {
+      const page = await pdf.getPage(pageNumber);
+      const viewport = page.getViewport({ scale: 1.0 });
+      const canvas = document.createElement('canvas');
+      const context = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-    canvas.width = viewport.width;
-    canvas.height = viewport.height;
+      canvas.width = viewport.width;
+      canvas.height = viewport.height;
 
-    await page.render({ canvasContext: context, viewport: viewport }).promise;
+      await page.render({ canvasContext: context, viewport: viewport }).promise;
 
-    const pngDataUrl = canvas.toDataURL('image/png');
-    extractedText += await transcribeImage(pngDataUrl);
+      const pngDataUrl = canvas.toDataURL('image/png');
+      extractedText += await transcribeImage(pngDataUrl);
+    }
+
+    return extractedText;
+} catch (err) {
+    console.error('Error in extractTextFromPDF:', err);
+    
+    const errorMessage = isError(err) ? err.message : 'An unknown error occurred';
+    throw new Error(`Error in extractTextFromPDF: ${errorMessage}`);
   }
-
-  return extractedText;
 }
 
 async function transcribeImage(pngDataUrl: string): Promise<string> {
-  const worker = await createWorker('eng');
-  const { data: { text } } = await worker.recognize(pngDataUrl);
-  await worker.terminate();
-  return text;
+  try {
+    const worker = await createWorker('eng');
+    const { data: { text } } = await worker.recognize(pngDataUrl);
+    await worker.terminate();
+    return text;
+} catch (err) {
+    console.error('Error in transcribeImage:', err);
+    
+    const errorMessage = isError(err) ? err.message : 'An unknown error occurred';
+    throw new Error(`Error in transcribeImage: ${errorMessage}`);
+  }
 }
